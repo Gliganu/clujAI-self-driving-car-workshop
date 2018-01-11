2273ae99b1e2c688da6b7f9ff18f9227aeac85fa
Gliga Bogdan <bogdan.gliga@telenav.com>
2018-01-11 23:00:48 +0200

    work in progress v6

--- answers/load_model_answer.py
+++ answers/load_model_answer.py
@@ -1,2 +1,2 @@
 model = load_model_from_json_and_weights("../provided_model/provided_model_architecture.json", 
-                                         "../provided_model/provided_model_weights.h5", )
+                                         "../provided_model/provided_model_weights.h5")
