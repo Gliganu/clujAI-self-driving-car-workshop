bd18d95b97b3b33d93105326a4fdf5bb85b45e13
Gliga Bogdan <gligabogdanh@gmail.com>
2018-03-20 23:12:48 +0200

    renaming + remove unused folder

--- answers/model_training_answer.py
+++ answers/model_training_answer.py
@@ -1,5 +1,5 @@
 model.fit(train_imgs, train_labels,
-      nb_epoch= 5,               
+      epochs = 5,               
       validation_data = (test_imgs, test_labels),
       callbacks = [ ModelCheckpoint("./model_temp.h5", monitor='val_loss', verbose= 1, save_best_only=True, mode='min')
                  ]
